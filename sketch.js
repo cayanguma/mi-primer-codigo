6d4ad3474b2e16abc844dd607f68b54120232ae0
cayanguma <160347113+cayanguma@users.noreply.github.com>
2024-02-17 14:52:44 -0500

    Mi primer codigo (resubido)
    a

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 600);
+  
+}
+
+function draw() {
+  background(112, 12, 255);
+  triangle (40,300,200,20,360, 300);
+  triangle (40,250,200,540,360, 40);
+}
